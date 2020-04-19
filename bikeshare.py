cfcee7e5208fae3eb2fe2046764a6bc1508d6dec
shanglin.yang <shanglin.yang@jd.com>
2020-04-18 22:18:08 -0700

    add constant variable comment on line 7 in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import json
 
+
+# The constant variables
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
